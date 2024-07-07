7de9b528f71868cad152e742c163be9d121ce447
Guilherme Rodrigues <oliveirack1@gmail.com>
2024-07-06 23:17:20 -0300

    creating class

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . "/../src/Modelo/Movie.php";
+
+echo "Bem vindo ao screen match do donguinha\n\n";
+
+$movie = new Movie();
+
+$movie->title = 'Vingadores';
+$movie->year = 1999;
+$movie->gender = 'Heroes';
+
+$movie->addMovieScore(2.4);
+$movie->addMovieScore(10);
+$movie->addMovieScore(8.3);
+
+echo $movie->averageScore();
+
+echo "\n";
+
+var_dump($movie);
