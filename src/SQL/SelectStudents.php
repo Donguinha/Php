3349a4eb048c2caac8bf54083b3bd0f1814acec7
Guilherme Rodrigues <oliveirack1@gmail.com>
2024-07-23 15:41:30 -0300

    fetch functions

--- src/SQL/SelectStudents.php
+++ src/SQL/SelectStudents.php
@@ -3,16 +3,18 @@
 namespace Src\SQL;
 
 use PDO;
-use Src\Model\Student;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $dataBasePath = __DIR__ . "/banco.sqlite";
 $pdo = new PDO("sqlite:" . $dataBasePath);
 
-$statement = $pdo->query("SELECT * FROM student;");
+$student =
+    $pdo
+        ->query("SELECT * FROM student WHERE name = 'Guilherme';")
+        ->fetch(PDO::FETCH_ASSOC);
+
 //$student  = $statement->fetch(PDO::FETCH_CLASS, Student::class);
-$student  = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 var_dump($student);
 //echo $student[2]['name'];
